86e3e34f0991f8b73de25563deebfb0a35c49f3e
Gastón <kaupisoft@gmail.com>
2023-03-12 17:24:36 -0300

    cambios en db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
-
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "./config";
 // crear la conexión a la base de datos
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  port: "3306",
-  password: "",
-  database: "expressdb",
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
 });
